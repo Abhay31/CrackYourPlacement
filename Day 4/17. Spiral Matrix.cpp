a26de9bf64c41ac66ca6d2389d1ddd9caf0cab15
Abhay31 <abhaysharma0983@gmail.com>
2023-07-06 22:55:18 +0530

    Completed Day-4 Questions

---
+++ Day 4/17. Spiral Matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int>ans;
+        int count = 0;
+        int total = n*m;
+        int srow = 0;
+        int scol = 0;
+        int erow = n-1;
+        int ecol = m-1;
+        while(count<total)
+        {
+            for(int i = scol; count<total && i<=ecol;i++)
+            {
+                ans.push_back(matrix[srow][i]);
+                count++;
+            }
+            srow++;
+            for(int i = srow;count<total && i<=erow;i++)
+            {
+                ans.push_back(matrix[i][ecol]);
+                count++;
+            }
+            ecol--;
+            for(int i = ecol;count<total && i>=scol;i--)
+            {
+                ans.push_back(matrix[erow][i]);
+                count++;
+            }
+            erow--;
+            for(int i = erow;count<total && i>=srow;i--)
+            {
+                ans.push_back(matrix[i][scol]);
+                count++;
+            }
+            scol++;
+        }
+        return ans;
+    }
+};
